42a3692099218cdd10cc4da0e165288295153ddb
Clive France <clivefrance@internetics.co.uk>
2013-07-02 10:16:18 +1000

    Added app_id to appirater

--- NoiseMeter/Support/Appirater/Appirater.m
+++ NoiseMeter/Support/Appirater/Appirater.m
@@ -50,7 +50,7 @@ NSString *const kAppiraterRatedCurrentVersion		= @"kAppiraterRatedCurrentVersion
 NSString *const kAppiraterDeclinedToRate			= @"kAppiraterDeclinedToRate";
 NSString *const kAppiraterReminderRequestDate		= @"kAppiraterReminderRequestDate";
 
-NSString *templateReviewURL = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=APP_ID";
+NSString *templateReviewURL = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=512411644";
 
 static NSString *_appId;
 static double _daysUntilPrompt = 30;
