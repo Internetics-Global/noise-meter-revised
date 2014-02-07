8e4f475c42bff63a4a2f3925573b1fc3e5976792
Liang Wang <wangbourne@gmail.com>
2014-02-07 16:32:13 +1000

    update

--- NoiseMeter/AppDelegate.m
+++ NoiseMeter/AppDelegate.m
@@ -39,7 +39,7 @@
     
     if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
         [[UITabBar appearance] setBarTintColor:[UIColor blackColor]];
-        [[UITabBar appearance] setTintColor:[UIColor colorWithRed:64.0/255 green:190.0/255 blue:200.0/255 alpha:1]];
+        [[UITabBar appearance] setTintColor:[UIColor colorWithRed:168.0/255 green:225.0/255 blue:251.0/255 alpha:1]];
     }
     
     
