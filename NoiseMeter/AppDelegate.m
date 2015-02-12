ecaf14b972fd8d45663709aec765de63e9bbaff3
Liang Wang <wangbourne@gmail.com>
2015-02-12 11:19:05 +1000

    add version/build check（2）

--- NoiseMeter/AppDelegate.m
+++ NoiseMeter/AppDelegate.m
@@ -145,9 +145,15 @@
 
 - (void) setupVersionReminder {
     
+    [iVersion sharedInstance].checkPeriod = 0;
+    
     [iVersion sharedInstance].applicationBundleID = @"com.internetics.noisedown";
     
     [iVersion sharedInstance].remoteVersionsPlistURL = @"https://s3-ap-southeast-2.amazonaws.com/noisedown/versions.plist";
+    [iVersion sharedInstance].localVersionsPlistPath = @"versions.plist";
+    
+    
+    
 }
 
 
