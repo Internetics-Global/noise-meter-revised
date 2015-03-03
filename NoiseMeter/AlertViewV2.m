4fdc79356b88e45e366c4d7dee6f2b931391ce7c
Liang Wang <wangbourne@gmail.com>
2015-03-03 11:53:12 +1000

    update

--- NoiseMeter/AlertViewV2.m
+++ NoiseMeter/AlertViewV2.m
@@ -111,7 +111,7 @@
     
     //step3
     if (iPhone5) {
-        frame = CGRectMake((320-150)/2, CGRectGetMaxY(_slider.frame) + 107, 150, 150);
+        frame = CGRectMake((320-150)/2, CGRectGetMaxY(_slider.frame) + 102, 150, 150);
     } else {
         frame =  CGRectMake((320-150)/2, CGRectGetMaxY(_slider.frame) + 87, 150, 150);
     }
