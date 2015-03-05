353b36af2062c53c73439254df1a50c6dbbfc3be
Liang Wang <wangbourne@gmail.com>
2015-03-06 09:52:17 +1000

    update

--- NoiseMeter/UI/Root/MeterView.m
+++ NoiseMeter/UI/Root/MeterView.m
@@ -337,7 +337,7 @@
     _captureMeterBaseView.layer.masksToBounds = YES;
     [_meterBackground addSubview:_captureMeterBaseView];
     
-    _captureMeterDesLabel = [[UILabel alloc] initWithFrame:CGRectMake(3, 8, K_Square_Width - 6, 20)];
+    _captureMeterDesLabel = [[UILabel alloc] initWithFrame:CGRectMake(3, 8, K_Square_Width - 6, 25)];
     _captureMeterDesLabel.textAlignment = NSTextAlignmentCenter;
     _captureMeterDesLabel.font = [UIFont fontWithName:K_Square_Font_Name size:20];
     _captureMeterDesLabel.textColor = [UIColor lightGrayColor];
@@ -347,7 +347,7 @@
     _captureMeterDesLabel.layer.masksToBounds = YES;
     [_captureMeterBaseView addSubview:_captureMeterDesLabel];
     
-    _captureMeterLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 25, K_Square_Width, 30)];
+    _captureMeterLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, K_Square_Width, 40)];
     _captureMeterLabel.textAlignment = NSTextAlignmentCenter;
     _captureMeterLabel.font = [UIFont fontWithName:K_Square_Font_Name size:K_Square_FontSize_Des];
     _captureMeterLabel.textColor = [UIColor lightGrayColor];
@@ -635,10 +635,18 @@
             _captureMeterLabel.text = [NSString stringWithFormat:@"%d", [_currentReading intValue]];
             
             if ([NSUserDefaultsHelper isContinuousMode]||[NSUserDefaultsHelper isSilentMode]) {
-                _captureMeterBaseView.hidden = YES;
+                _captureMeterImageView.hidden = YES;
+                _captureMeterDesLabel.frame = CGRectMake(3, 11, K_Square_Width - 6, 25);
+                _captureMeterLabel.frame = CGRectMake(0, 25, K_Square_Width, 40);
+                _captureMeterBaseView.userInteractionEnabled = NO;
+
             } else {
-                _captureMeterBaseView.hidden = NO;
+                _captureMeterImageView.hidden = NO;
+                _captureMeterDesLabel.frame = CGRectMake(3, 8, K_Square_Width - 6, 25);
+                _captureMeterLabel.frame = CGRectMake(0, 20, K_Square_Width, 40);
+                _captureMeterBaseView.userInteractionEnabled = YES;
             }
+            _captureMeterBaseView.hidden = NO;
             
             _infoMeterBaseView.hidden = YES;
         
@@ -731,11 +739,24 @@
     if (([[NMDecibelLogger defaultLogger] logging]) && ([[NMDecibelLogger defaultLogger] playingAlarm] == FALSE)) {
         _cancelButton.hidden = YES;
         float lastPeakVal = [NSUserDefaultsHelper lastNoisePeakValue];
-        if ((int)lastPeakVal == 0 || [NSUserDefaultsHelper isContinuousMode]||[NSUserDefaultsHelper isSilentMode]) {
+        if ((int)lastPeakVal == 0) {
           _captureMeterBaseView.hidden = YES;
         } else {
           _captureMeterBaseView.hidden = NO;
             _captureMeterLabel.text = [NSString stringWithFormat:@"%d",(int)lastPeakVal];
+        }
+        
+        if ([NSUserDefaultsHelper isContinuousMode]||[NSUserDefaultsHelper isSilentMode]) {
+            _captureMeterImageView.hidden = YES;
+            _captureMeterDesLabel.frame = CGRectMake(3, 11, K_Square_Width - 6, 25);
+            _captureMeterLabel.frame = CGRectMake(0, 25, K_Square_Width, 40);
+            _captureMeterBaseView.userInteractionEnabled = NO;
+            
+        } else {
+            _captureMeterImageView.hidden = NO;
+            _captureMeterDesLabel.frame = CGRectMake(3, 8, K_Square_Width - 6, 25);
+            _captureMeterLabel.frame = CGRectMake(0, 20, K_Square_Width, 40);
+            _captureMeterBaseView.userInteractionEnabled = YES;
         }
         
     }
